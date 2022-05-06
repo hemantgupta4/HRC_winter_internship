c94163d3dff5303dc9e10b75f9b2fc46008b8169
hemantgupta4 <78254044+hemantgupta4@users.noreply.github.com>
2022-05-06 11:01:33 +0530

    Add files via upload

---
+++ Web App/backend/src/main/java/database/Database.java
@@ -0,0 +1,24 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    public static Connection getConnection() throws SQLException {
+         String url = "jdbc:mysql://localhost:3306/grey_goose";
+       
+        String user = "root";
+        String pass = "qwasz0987#";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Connection conn = DriverManager.getConnection(url, user, pass);
+
+        return conn;
+    }
+}
